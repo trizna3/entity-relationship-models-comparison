9cc53a093508869365ecb3be6bd1afc3aba98b50
Adam Trizna <adam.trizna32@gmail.com>
2020-02-16 21:38:58 +0100

    Mapping object commentaries added.

--- src/comparing/Mapping.java
+++ src/comparing/Mapping.java
@@ -5,8 +5,15 @@ import entityRelationshipModel.EntitySet;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Object representing mapping of entity sets between two entity relationship model.
+ */
 public class Mapping {
 
+    /**
+     * Mapping.
+     * If es1 and es2 are paired, both (es1,es2) and (es2,es1) will be in the data
+     */
     private Map<EntitySet,EntitySet> data;
 
     private Map<EntitySet, EntitySet> getData() {
@@ -16,6 +23,11 @@ public class Mapping {
         return data;
     }
 
+    /**
+     * Add mapping pair. (both entries in the data map)
+     * @param entitySet1
+     * @param entitySet2
+     */
     public void map(EntitySet entitySet1, EntitySet entitySet2) {
         if (entitySet1 == null || entitySet2 == null) {
             throw new IllegalArgumentException("mapped entity set cannot be null");
@@ -24,10 +36,27 @@ public class Mapping {
         getData().put(entitySet2,entitySet1);
     }
 
+    /**
+     * Remove mapping pair. (both key and value)
+     * @param entitySet
+     */
     public void unmap(EntitySet entitySet) {
-        // unmapni jeho ako kluc aj ako value
+        if (entitySet == null) {
+            throw new IllegalArgumentException("illegal unmapping request - entity set is null");
+        }
+        if (getData().containsKey(entitySet)) {
+            for (EntitySet key : getData().keySet()) {
+                if (entitySet.equals(key) || entitySet.equals(getData().get(key))) {
+                    getData().remove(key);
+                }
+            }
+        }
     }
 
+    /**
+     * @param entitySet
+     * @return paired entity set of given entity set
+     */
     public EntitySet getImage(EntitySet entitySet) {
         if (entitySet == null) {
             throw new IllegalArgumentException("illegal mapping image request - entity set is null");
