bbd355d35a4eedbf57460f735b9806fa9fcd326f
Adam Trizna <adam.trizna32@gmail.com>
2020-03-17 20:47:54 +0100

    AddEntitySet transformation modified. Gets whole entity set instance as input, doesn't create one.

--- src/transformations/types/Transformation_AddEntitySet.java
+++ src/transformations/types/Transformation_AddEntitySet.java
@@ -15,48 +15,28 @@ import java.util.List;
  */
 public class Transformation_AddEntitySet extends Transformation {
 
-    public static final String ENTITY_SET_NAME = "ENTITY_SET_NAME";
-    public static final Class<String> ENTITY_SET_NAME_CLASS = String.class;
-
-    public static final String ENTITY_SET_ATTRIBUTES = "ENTITY_SET_ATTRIBUTES";
-    public static final Class<List> ENTITY_SET_ATTRIBUTES_CLASS = List.class;
-
-    /**
-     * Created entity set.
-     */
-    private EntitySet entitySet;
+    public static final String ENTITY_SET = "ENTITY_SET";
+    public static final Class<EntitySet> ENTITY_SET_CLASS = EntitySet.class;
 
     public Transformation_AddEntitySet() {
-        parameterNames = new String[]{ENTITY_SET_NAME,ENTITY_SET_ATTRIBUTES};
+        parameterNames = new String[]{ENTITY_SET};
     }
 
     @Override
     public void execute(EntityRelationshipModel model) {
-        if (this.entitySet != null) {
+        EntitySet entitySet = getEntitySet();
+        if (model.contains(entitySet)) {
             throw new IllegalStateException("transformation already executed!");
         }
-        this.entitySet = new EntitySet(getEntitySetName(),getEntitySetAttributes());
         model.addEntitySet(entitySet);
     }
 
     @Override
     public void setToOriginalState(EntityRelationshipModel model) {
-        model.removeEntitySet(entitySet);
-    }
-
-    public String getEntitySetName() {
-        return ENTITY_SET_NAME_CLASS.cast(parameterMap.get(ENTITY_SET_NAME));
-    }
-
-    public List<String> getEntitySetAttributes() {
-        return ENTITY_SET_ATTRIBUTES_CLASS.cast(parameterMap.get(ENTITY_SET_ATTRIBUTES));
+        model.removeEntitySet(getEntitySet());
     }
 
     public EntitySet getEntitySet() {
-        return entitySet;
-    }
-
-    public void setEntitySet(EntitySet entitySet) {
-        this.entitySet = entitySet;
+        return ENTITY_SET_CLASS.cast(parameterMap.get(ENTITY_SET));
     }
 }
