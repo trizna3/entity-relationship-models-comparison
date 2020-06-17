f616aaaaf866ca98bba60fa9f5fdb4fb1782bec2
trizna3 <adam.trizna32@gmail.com>
2020-06-17 20:53:39 +0200

    Enums added

---
+++ src/common/Enums.java
@@ -0,0 +1,12 @@
+package common;
+
+public class Enums {
+
+	public static final String CARDINALITY_ONE = "CARDINALITY_ONE";
+	
+	public static final String CARDINALITY_MANY = "CARDINALITY_MANY";
+	
+	public static final String ROLE_SUPER = "ROLE_SUPER";
+	
+	public static final String ROLE_SUB = "ROLE_SUB";
+}
