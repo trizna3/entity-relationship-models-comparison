a6d62123766a3f47a397d1e1f91bcc4d1f8fc28f
trizna3 <adam.trizna32@gmail.com>
2020-09-17 22:01:16 +0200

    MappingSearch: transformation analysis introduced.

--- src/transformations/TransformationAnalyst.java
+++ src/transformations/TransformationAnalyst.java
@@ -1,5 +1,6 @@
 package transformations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import comparing.Mapping;
@@ -22,6 +23,73 @@ public class TransformationAnalyst {
 	 * @return
 	 */
 	public static List<Transformation> getPossibleTransformations(Mapping mapping) {
-		throw new UnsupportedOperationException();
+		List<Transformation> result = new ArrayList<>();
+
+		result.addAll(getPossibleExtractAttributeToOwnEntitySetTransformations(mapping));
+		result.addAll(getPossibleMoveAttributeToIncidentEntitySetTransformations(mapping));
+		result.addAll(getPossibleMoveAttributeToIncidentAssociationTransformations(mapping));
+		result.addAll(getPossibleRebindMNTo1NN1Transformations(mapping));
+		result.addAll(getPossibleRebind1NN1ToMNTransformations(mapping));
+		result.addAll(getPossibleGeneralizationToAssociationTransformations(mapping));
+		result.addAll(getPossibleContract11AssociationTransformations(mapping));
+		result.addAll(getPossibleRebindNaryAssociationTransformations(mapping));
+
+		return result;
+	}
+
+	private static List<Transformation> getPossibleExtractAttributeToOwnEntitySetTransformations(Mapping mapping) {
+//		EXTRACT_ATTR_TO_OWN_ENTITY_SET - ATTRIBUTE,SOURCE_ENTITY_SET,DEST_ENTITY_SET - ATTRIBUTE,DEST_ENTITY_SET,SOURCE_ENTITY_SET
+
+		// unsupported yet
+		return new ArrayList<>();
+	}
+
+	private static List<Transformation> getPossibleMoveAttributeToIncidentEntitySetTransformations(Mapping mapping) {
+//		MOVE_ATTR_TO_INCIDENT_ENTITY_SET - ATTRIBUTE,ASSOCIATION,ENTITY_SET - ATTRIBUTE,ASSOCIATION,ENTITY_SET
+
+		// unsupported yet
+		return new ArrayList<>();
+	}
+
+	private static List<Transformation> getPossibleMoveAttributeToIncidentAssociationTransformations(Mapping mapping) {
+//		MOVE_ATTR_TO_INCIDENT_ASSOCIATION - ATTRIBUTE,ENTITY_SET,ASSOCIATION - ATTRIBUTE,ASSOCIATION,ENTITY_SET
+
+		// unsupported yet
+		return new ArrayList<>();
+	}
+
+	private static List<Transformation> getPossibleRebindMNTo1NN1Transformations(Mapping mapping) {
+//		REBIND_MN_TO_1NN1 - ASSOCIATION - ASSOCIATION_1,ASSOCIATION_2,ENTITY_SET
+
+		// unsupported yet
+		return new ArrayList<>();
+	}
+
+	private static List<Transformation> getPossibleRebind1NN1ToMNTransformations(Mapping mapping) {
+//		REBIND_1NN1_TO_MN - ASSOCIATION_1,ASSOCIATION_2,ENTITY_SET - ASSOCIATION
+
+		// unsupported yet
+		return new ArrayList<>();
+	}
+
+	private static List<Transformation> getPossibleGeneralizationToAssociationTransformations(Mapping mapping) {
+//		GENERALIZATION_TO_11_ASSOCIATION - GENERALIZATION - ASSOCIATION, GENERALIZATION
+
+		// unsupported yet
+		return new ArrayList<>();
+	}
+
+	private static List<Transformation> getPossibleContract11AssociationTransformations(Mapping mapping) {
+//		CONTRACT_11_ASSOCIATION - ASSOCIATION - ENTITY_SET, ASSOCIATION, TRANSFORMABLE_LIST
+
+		// unsupported yet
+		return new ArrayList<>();
+	}
+
+	private static List<Transformation> getPossibleRebindNaryAssociationTransformations(Mapping mapping) {
+//		REBIND_NARY_ASSOCIATION - ASSOCIATION - ENTITY_SET, ASSOCIATION
+
+		// unsupported yet
+		return new ArrayList<>();
 	}
 }
